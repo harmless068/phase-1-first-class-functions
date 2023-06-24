4e74313031994e7757376cb611b3e7e4bd273344
osman <harmless068@gmail.com>
2023-06-24 22:58:28 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+  callback ();
+}
+function returnsANamedFunction(){
+  function namedFunction() {
+    console.log("Named function called!");
+}
+return namedFunction;
+}
+function returnsAnAnonymousFunction(){
+  return function() {
+    console.log("Anonymous function called!");
+  }; 
+}
